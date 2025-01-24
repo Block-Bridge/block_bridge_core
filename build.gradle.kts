732f811ddf708d2247f491b2188a3c8768a4b452
QuickScythe <QuickScythe@gmail.com>
2025-01-24 12:07:40 -0600

    build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,19 @@
+val coreVersion:        String by project
+val jsonVersion:        String by project
+val buildStatus:        String by project
+val buildNumber:        String by project
+
+val junitVersion:       String by project
+val apacheVersion:      String by project
+
+val logbackVersion:     String by project
+val group:              String by project
+
 plugins {
     id("java")
 }
 
-group = "me.quickscythe"
-version = "1.0-SNAPSHOT"
+version = "$coreVersion-$buildStatus.$buildNumber"
 
 repositories {
     mavenCentral()
@@ -12,6 +22,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("commons-io:commons-io:${apacheVersion}")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.json:json:$jsonVersion")
 }
 
 tasks.test {
