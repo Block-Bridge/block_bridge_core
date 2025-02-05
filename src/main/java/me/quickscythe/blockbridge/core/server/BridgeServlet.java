fab0932a677b90f25bbe81f5a3f235416a269b61
QuickScythe <QuickScythe@gmail.com>
2025-02-05 04:36:17 -0600

    Better server handling by Jetty 3

--- src/main/java/me/quickscythe/blockbridge/core/server/BridgeServlet.java
+++ src/main/java/me/quickscythe/blockbridge/core/server/BridgeServlet.java
@@ -1,16 +1,19 @@
 package me.quickscythe.blockbridge.core.server;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
 
 public class BridgeServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    private final BridgeServer server;
+
+    public BridgeServlet(BridgeServer server) {
+        this.server = server;
     }
+
+    public BridgeServer server() {
+        return server;
+    }
+
+
+
 }
